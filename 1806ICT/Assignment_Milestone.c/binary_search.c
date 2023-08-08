9ec3286d5869b36b2d1822cff50172fc567dcbdd
Manykeystrikes <adbart@live.com.au>
2023-08-08 13:54:55 +1000

    updated

---
+++ 1806ICT/Assignment_Milestone.c/binary_search.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+# include <stdbool.h>
+
+int n, r1, r2, range;
+int 
+int inums;
+bool use_count = false, testing = false 
+int binary_search(int v); // return -1 if not there or index in nums of ther first one
+int find(int v);
+int add(int v);
+int delete(int v);
+int pred(int v);
+int scc(int v);
+int nmin();
+int max();
+
+
+int main (int argc, char * argv [])
+{
+}
+
+ int  int index = binary_search(v);
+ if (index == -1)
+        return 0,
+        // count duplicates and return
+ }
