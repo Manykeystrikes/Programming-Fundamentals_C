66dcf46f3db2a71d56eb8550d4204d0d9fc49c17
Manykeystrikes <adbart@live.com.au>
2023-08-23 22:29:15 +1000

    update 23

---
+++ 1806ICT/Week_3_Arrays_Strings/Lab_1/p01_array_min_max.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*. Write a program that reads in the count of array elements and the actual array elements (all of which 
+are integers). The program then prints out the minimum and maximum values in the given array.
+Sample Run:
+Input           Output
+3 4 8 5         Min = 4, Max = 8
+5 14 2 5 13 9   Min = 2, Max = 14*/
+
+int main()
+{
+    int values[100];
+    int n;
+    
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &values[i]);
+
+    int min = values[0];
+    int max = values[0];
+    for (int i= 1; i < n; i++)
+    {
+        if (values[i] < min)
+        {
+            min = values[i];
+        }
+        if (values[i] > max)
+        {
+            max = values[i];
+        }
+    }
+        printf("Min = %d, Max = %d\n", min, max);
+
+
+
+    return 0;
+
+
+}
